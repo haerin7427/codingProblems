b4d9d857d5192d197bee53d7471922965d795203
haerin7427 <hae7427@gmail.com>
2021-07-08 17:50:01 +0900

    week1 D1 odd complete

--- src/week1_D1_9.java
+++ src/week1_D1_9.java
@@ -1,6 +1,6 @@
 
 // https://leetcode.com/problems/two-city-scheduling/
-// time : 
+// time : 55m
 
 public class week1_D1_9 {
 
@@ -13,24 +13,48 @@ public class week1_D1_9 {
 
             
             //Array 만들기
-            int[][] gapArray = new int[limitNum][2];
-            for(int i = 0; i < limitNum; i++){
-                gapArray[i][0] = 2*i;
+            int[][] gapArray = new int[costs.length][2];
+            for(int i = 0; i < costs.length; i++){
+                gapArray[i][0] = i;
                 gapArray[i][1] = Math.abs(costs[i][0]-costs[i][1]);
             }
             
+            
+            
             //sort
+            Arrays.sort(gapArray, new Comparator<int[]>(){
+                @Override 
+                public int compare(int[] num1, int[] num2){
+                    if(num1[1] == num2[1])
+                        return Integer.compare(num1[0],num2[0]);
+                    else
+                        return Integer.compare(num1[1],num2[1]);
+                }
+            });
+            
+            // for(int i =0; i < gapArray.length;i++ )
+            //         System.out.println(i+"th : "+gapArray[i][1]+" ("+gapArray[i][0]+") ");
+        
             
             
             //decide
-            for(int i =0; i < gapArray.length; i++){
+            for(int i = gapArray.length-1; i > -1; i--){
                 
                 
-                if( (costs[gapArray[i][0]] < costs[gapArray[i][0]+1] && aCount < limitNum) || (costs[i] > costs[i+1] && bCount >= limitNum){
+                if( ((costs[gapArray[i][0]][0] < costs[gapArray[i][0]][1]) && (aCount < limitNum)) || ((costs[gapArray[i][0]][0] > costs[gapArray[i][0]][1]) && (bCount >= limitNum)) ){
+                    aCount++;
                     
+                    totalCost += costs[gapArray[i][0]][0];
+                    
+                }else{
+                    bCount++;
+                    totalCost += costs[gapArray[i][0]][1];
                 }
             }
-        }
+            
+            return totalCost;
+         }
+            
     }
     
 }
