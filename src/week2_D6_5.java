2168862044636f35446e01964b376bf4d12caf12
haerin7427 <hae7427@gmail.com>
2021-07-19 13:44:10 +0900

    Week2 Day6 No.5 complete!!

---
+++ src/week2_D6_5.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+
+// https://www.acmicpc.net/problem/10867
+// time : 25m
+public class week2_D6_5 {
+    public static void main(String[] args) throws IOException{
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        String[] num = br.readLine().split(" ");
+        br.close();
+        ArrayList<Integer> arr = new ArrayList<>();
+
+        for(String item : num){
+            if(arr.contains(Integer.parseInt(item)) == false)
+                arr.add(Integer.parseInt(item));
+        }
+
+
+        arr.sort(Comparator.naturalOrder());
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        for(int i = 0; i < arr.size(); i++)
+            bw.write(arr.get(i) + " ");
+    
+        bw.flush();
+        bw.close();
+        
+    }
+}
