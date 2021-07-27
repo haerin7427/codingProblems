bcc7762628ce13ee8482c977c1aa579d1554bd27
haerin7427 <hae7427@gmail.com>
2021-07-27 10:27:02 +0900

    Week2 Day6 No.3,7,9 complete!!

--- src/week2_D6_9.java
+++ src/week2_D6_9.java
@@ -2,6 +2,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+// https://www.acmicpc.net/problem/17215
+// time : ...
+
 public class week2_D6_9 {
 
     public static void main(String[] args) throws IOException {
@@ -14,53 +17,56 @@ public class week2_D6_9 {
 
             if('0' < str.charAt(idx) && str.charAt(idx)  <= '9'){
                 idx+=1;
+
                 if('0' < str.charAt(idx) && str.charAt(idx)  <= '9'){
                     score += Integer.parseInt(str.substring(idx-1, idx));
                     score += Integer.parseInt(str.substring(idx, idx+1));
                 }else if(str.charAt(idx) == 'P' ){
-
-                    if(frame != 9){
+                    score += 10;
+                    if('0' < str.charAt(idx+1) && str.charAt(idx+1)  <= '9'){
+                        score += Integer.parseInt(str.substring(idx+1, idx+2));
+                    }else if(str.charAt(idx+1) == 'S'){
                         score += 10;
-                        if('0' < str.charAt(idx+1) && str.charAt(idx+1)  <= '9'){
-                            score += Integer.parseInt(str.substring(idx+1, idx+2));
-                        }else if(str.charAt(idx+1) == 'S'){
-                            score += 10;
-                        }
-                    }else{
-                        
                     }
                     
+                }else if(str.charAt(idx) == '-' ){
+                    score += Integer.parseInt(str.substring(idx-1, idx));
                 }
                 idx+=1;
+            }else if(str.charAt(idx) == '-'){
+                idx+=1;
+                if('0' < str.charAt(idx) && str.charAt(idx)  <= '9'){
+                    score += Integer.parseInt(str.substring(idx, idx+1));
+                }else if(str.charAt(idx) == 'P' ){
+                    score += 10;
+                    if('0' < str.charAt(idx+1) && str.charAt(idx+1)  <= '9'){
+                        score += Integer.parseInt(str.substring(idx+1, idx+2));
+                    }else if(str.charAt(idx+1) == 'S'){
+                        score += 10;
+                    }
+                }
+                idx+=1;
+
             }else if(str.charAt(idx) == 'S'){
                 score += 10;
 
-                if(frame == 9){
-
-                }else{
-                    if(str.charAt(idx+2) == 'P')
-                        score += 10;
-                    else {
-                        for(int i = 1; i < 3 ; i++){
-                            if('0' < str.charAt(idx+i) && str.charAt(idx+i) <= '9')
-                                score += Integer.parseInt(str.substring(idx+i, idx+i));
-                            else if(str.charAt(idx+2) == 'S')
-                                score += 10;
-                        }
-
+                if(str.charAt(idx+2) == 'P')
+                    score += 10;
+                else {
+                    for(int i = 1; i < 3 ; i++){
+                        if('0' < str.charAt(idx+i) && str.charAt(idx+i) <= '9')
+                            score += Integer.parseInt(str.substring(idx+i, idx+i+1));
+                        else if(str.charAt(idx+i) == 'S')
+                            score += 10 ;
                     }
+
                 }
 
-                
+                idx+=1;
             }
-
         }
 
-     
-
-            
-        
-
+        System.out.println(score);
 
     }
     
