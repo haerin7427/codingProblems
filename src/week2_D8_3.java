ba5699fcf1b147a516a0c912a9268b34d258973b
haerin7427 <43716537+haerin7427@users.noreply.github.com>
2021-07-28 22:25:52 +0900

    Week2 Day8 No.3,5 complete!!

--- src/week2_D8_3.java
+++ src/week2_D8_3.java
@@ -1,9 +1,38 @@
 
 
 // https://leetcode.com/problems/reverse-linked-list/
-// time : 5m + ...
+// time : 40m
 
 
 public class week2_D8_3 {
+
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     *     int val;
+     *     ListNode next;
+     *     ListNode() {}
+     *     ListNode(int val) { this.val = val; }
+     *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+
+    class Solution {
+        public ListNode reverseList(ListNode head) {
+            
+            ListNode curr = null;
+            ListNode temp = head;
+            ListNode prev = null;
+            
+            while(temp != null){
+                prev = curr;
+                curr = temp;
+                temp = temp.next;
+                curr.next = prev;
+            }
+            
+            return curr;
+        }
+    }
 
 }
