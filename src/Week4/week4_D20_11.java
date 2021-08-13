dd2359d20b90dbed774fba8e48015914eab525fb
haerin7427 <hae7427@gmail.com>
2021-08-13 15:52:35 +0900

    Week4 Day20 No.11 ing

--- src/Week4/week4_D20_11.java
+++ src/Week4/week4_D20_11.java
@@ -6,10 +6,10 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 // https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AWFUu2_aAg4DFAVB&categoryId=AWFUu2_aAg4DFAVB&categoryType=CODE&&&
-// timeL 1:55
+// timeL 120m
 // Create by haerin on 2021/08/13
 public class week4_D20_11 {
-
+ 
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int testCase = Integer.parseInt(br.readLine());
@@ -19,14 +19,24 @@ public class week4_D20_11 {
             int count =0;
             int N = Integer.parseInt(br.readLine()); 
             N+=1;
-            int[][] arr = new int[2][N];
-            arr[0][0] = Integer.parseInt(br.readLine());
+            info[] list = new info[N];
+            list[0] = new info(Integer.parseInt(br.readLine()), -1);
+
     
             for(int i=1; i < N; i++){
                 StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-                arr[0][i] = Integer.parseInt(st.nextToken());
-                arr[1][i] = Integer.parseInt(st.nextToken());
-                count += checkMoney(arr, i);
+                list[i] = new info(Integer.parseInt(st.nextToken()),  Integer.parseInt(st.nextToken()));
+
+                int bossNum = list[i].boss;
+
+                while(bossNum != -1){
+                    if(list[bossNum].price < list[i].price){
+                        count++;
+                        list[bossNum].price = list[i].price;
+                        bossNum = list[bossNum].boss; 
+                    }else
+                        break;
+                }
             }
     
             sb.append("#"+idx+" "+count+"\n");
@@ -34,21 +44,20 @@ public class week4_D20_11 {
         }
         System.out.println(sb);
     }
-    
-    private static int checkMoney (int[][] arr, int idx){
-    
-        int boss = arr[1][idx];
-    
-        if(arr[0][boss] < arr[0][idx]){
-            arr[0][boss] = arr[0][idx];
-    
-            if(boss != 0)
-                return 1 + checkMoney(arr, boss);
-            else
-                return 1;
-        }else
-            return 0;
+
+    static class info{
+            
+        int price;
+        int boss;
+
+        public info(int a, int b){
+            price =a;
+            boss =b;
+        }
     }
+    
+   
+
 }
 
 
--- src/test.java
+++ src/test.java
@@ -1,20 +1,47 @@
+import java.io.IOException;
+import java.util.Scanner;
 
+// 지우지 말자
+class PTable {
+    int pay;
+    int sv;
+     
+    public PTable(int pay, int sv) {
+        this.pay = pay;
+        this.sv = sv;
+    }
+}
+ 
 
 public class test {
+	public static void main(String[] args) throws IOException {
 
-    public static void main(String[] args) {
-        String testStr = "Baaa";
-
-        StringBuilder sb = new StringBuilder();
-            for(int i = 0 ; i < 3; i++)
-                sb.append('a');
-
-        
-
-        if(sb.equals(testStr.substring(1,4)))
-            System.out.println("true");
-        else
-            System.out.println("false");
-    }
-    
+		Scanner sc = new Scanner(System.in);
+        int T;
+        T=sc.nextInt();
+		for(int test_case = 1; test_case <= T; test_case++)
+        {
+            int n = sc.nextInt(); // 직원수
+             
+            PTable[] pt = new PTable[n+1];
+            pt[0] = new PTable(sc.nextInt(), 0); // 다슬이의 첫연봉
+            int inc = 0;
+            for (int i = 1; i <= n; i++) {
+                pt[i] = new PTable(sc.nextInt(), sc.nextInt());
+                int idx = i;
+                while (idx > 0) {
+                    if(pt[pt[idx].sv].pay <= pt[idx].pay) {
+                        pt[pt[idx].sv].pay += pt[i].pay;  // 신입사원 연봉만큼 인상한다.
+                        inc++;
+                        idx = pt[idx].sv;
+                    } else {
+                        break;
+                    }
+                }
+            }
+             
+            System.out.println("#" + test_case + " " + inc);// 연봉 인상을 받은 직원수의 합
+ 
+        }
+	}
 }
