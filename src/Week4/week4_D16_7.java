250cb23c16ef6c4fd07bc037ef595fb489ceba62
haerin7427 <hae7427@gmail.com>
2021-08-24 16:17:50 +0900

    Week4 Day16 No,3,5,7,9 complete!!

--- src/Week4/week4_D16_7.java
+++ src/Week4/week4_D16_7.java
@@ -2,7 +2,7 @@ package Week4;
 
 // https://leetcode.com/problems/uncrossed-lines/
 // time: 15m
-// Create by haerin on 2021/08/24
+// Create by haerin on 2021/08/24 + Discussion
 
 public class week4_D16_7 {
 
@@ -14,7 +14,7 @@ public class week4_D16_7 {
             
             for(int x=1; x <=n; x++){
                 for(int y=1; y <=m; y++){
-                    
+                    // dp[x][y]는 nums1[0~x-1]까지의 수와 nums2[0~y-1]까지의 수 조합으로 그을 수 있는 선의 개수를 저장
                     if(nums1[x-1] == nums2[y-1]){
                         dp[x][y] = 1 + dp[x-1][y-1];
                     }else{
