ede060580376bf557f3ac23b647f35780b4d1eaf
haerin7427 <hae7427@gmail.com>
2021-08-25 14:54:50 +0900

    Week4 Day17 No.3,5,9 complete!!

--- src/Week4/week4_D17_3.java
+++ src/Week4/week4_D17_3.java
@@ -6,8 +6,8 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 // https://www.acmicpc.net/problem/11052
-// time: 35m +
-// Create by haerin on 2021/08/24
+// time: 85m
+// Create by haerin on 2021/08/25
 
 public class week4_D17_3 {
 
@@ -15,36 +15,19 @@ public class week4_D17_3 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int card = Integer.parseInt(br.readLine());
-            
-        int[] costs = new int[card];
+        int[] costs = new int[card+1];
+        int[] d = new int[card+1];
+        
         StringTokenizer st = new StringTokenizer(br.readLine()," ");
-        for(int i=0; i<card; i++)
+        for(int i=1; i<=card; i++)
             costs[i] = Integer.parseInt(st.nextToken());
 
-        int total=0;
+        // x: 필요한 카드 개수, y: 팩으로 사는 카드 수
+        for(int x=1; x<=card; x++)
+            for(int y=1; y<=x; y++)
+                d[x] = Math.max(d[x], d[x - y] + costs[y]);
 
-        while(card != 0){
-
-            float max = (float)costs[0]; int idx=0;
-            for(int i=1; i<card; i++){
-                float value = (float)costs[i]/(i+1);
-                if(max < value ){
-                    max = value;
-                    idx=i;
-                }
-            }
-
-            //int num = card / (idx+1);
-            if(card==1){
-                total += costs[0];
-                card = 0;
-            }else{
-                total += (card / (idx+1)) * costs[idx];
-                card = card % (idx+1);
-            } 
-        }
-
-        System.out.println(total);
+        System.out.println(d[card]);
     }
     
 }
---
+++ src/Week4/week4_D17_7.java
@@ -0,0 +1,9 @@
+package Week4;
+
+// https://leetcode.com/problems/brick-wall/
+// time: 
+// Create by haerin on 2021/08/25
+
+public class week4_D17_7 {
+
+}
