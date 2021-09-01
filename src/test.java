4781a85e15b802098be3df984cf65a73cf702133
haerin7427 <43716537+haerin7427@users.noreply.github.com>
2021-09-01 22:15:45 +0900

    add test.java

--- src/test.java
+++ src/test.java
@@ -1,20 +1,65 @@
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class test {
 
-    public static void main(String[] args) {
-        String testStr = "Baaa";
+    public static class treeNode{
+        int val;
+        treeNode left;
+        treeNode right;
 
-        StringBuilder sb = new StringBuilder();
-            for(int i = 0 ; i < 3; i++)
-                sb.append('a');
+        public treeNode(int v){
+            this.val = v;
+        }
+    }
+
+    public static void insertNode(treeNode root, int num){
+
+        if(root.val < num){
+            if(root.right == null){
+                treeNode newNode = new treeNode(num);
+                root.right = newNode;
+            }else
+                insertNode(root.right, num);
+        }else{
+            if(root.left == null){
+                treeNode newNode = new treeNode(num);
+                root.left = newNode;
+            }else
+                insertNode(root.left, num);
+        }
+        
+    }
+
+    public static void printNode(treeNode root, StringBuilder st){
+
+        if(root.left != null)
+            printNode(root.left, st);
+
+        if(root.right != null)
+            printNode(root.right, st);
+        
+        st.append(root.val + " ");
 
         
+    }
 
-        if(sb.equals(testStr.substring(1,4)))
-            System.out.println("true");
-        else
-            System.out.println("false");
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+        int N = Integer.parseInt(input);
+        treeNode root = new treeNode(N);
+
+        while((input = br.readLine()) != null && !input.equals("")){
+            insertNode(root, Integer.parseInt(input));
+        }
+
+        StringBuilder st = new StringBuilder();
+
+        printNode(root, st);
+        System.out.println(st);
     }
     
 }
