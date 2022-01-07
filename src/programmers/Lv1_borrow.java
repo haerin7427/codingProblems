80589b43948bbb69e91f6cffe4a396d2883ea3c2
haerin7427 <43716537+haerin7427@users.noreply.github.com>
2022-01-08 00:05:37 +0900

    LV1 borrow complete!!

---
+++ src/programmers/Lv1_borrow.java
@@ -0,0 +1,39 @@
+package programmers;
+
+//  https://programmers.co.kr/learn/courses/30/lessons/42862
+// title : 체육복
+// time : 10m
+// Create by haerin on 2022/01/08
+
+public class Lv1_borrow {
+    class Solution {
+        public int solution(int n, int[] lost, int[] reserve) {
+            int answer = 0;
+            int[] students = new int[n];
+            for(int i=0; i < n; i++)
+                students[i] = 1;
+            for(int lostStudent : lost)
+                students[lostStudent-1] -= 1;
+            
+            for(int reserveStudent : reserve)
+                students[reserveStudent-1] += 1;
+            
+            for(int i=0 ; i<n; i++){
+                if(students[i] == 0){
+                    if( i != 0 && students[i-1] > 1){
+                        students[i] = 1;
+                        students[i-1] = 1; 
+                    }else if(i != n-1 && students[i+1] > 1){
+                        students[i] = 1;
+                        students[i+1] = 1; 
+                    }
+                }
+                
+                if(students[i]  > 0)
+                    answer+=1;
+            }
+                
+            return answer;
+        }
+    }
+}
