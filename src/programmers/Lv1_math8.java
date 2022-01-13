abafc002cc4d57944557117f439a9232bc0aea7c
haerin7427 <43716537+haerin7427@users.noreply.github.com>
2022-01-14 00:44:36 +0900

    LV1 math8 edit comment!!

--- src/programmers/Lv1_math8.java
+++ src/programmers/Lv1_math8.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 // https://programmers.co.kr/learn/courses/30/lessons/68644
 // title : 두 개 뽑아서 더하기
 // time : 11m
-// Create by haerin on 2022/01/4
+// Create by haerin on 2022/01/13
 
 public class Lv1_math8 {
 
