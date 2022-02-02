e9cea182e6c219e2972e1f05eaad717509d714be
haerin7427 <43716537+haerin7427@users.noreply.github.com>
2022-02-02 21:08:31 +0900

    LV1 mid math17 complete!!

---
+++ src/programmers/Lv1_math17.java
@@ -0,0 +1,33 @@
+package programmers;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+// https://programmers.co.kr/learn/courses/30/lessons/12910
+// title : 나누어 떨어지는 숫자 배열
+// time : 10m
+// Create by haerin on 2022/02/02
+
+public class Lv1_math17 {
+
+    
+
+class Solution {
+    public int[] solution(int[] arr, int divisor) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        for(int num : arr){
+            if(num % divisor == 0)
+                list.add(num);
+        }
+        if(list.size() == 0)
+            list.add(-1);
+        
+        int[] answer = new int[list.size()];
+        int idx = 0;
+        for(int num : list)
+            answer[idx++] = num;
+        Arrays.sort(answer);
+        return answer;
+    }
+}
+    
+}
