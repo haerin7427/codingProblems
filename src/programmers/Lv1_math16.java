84b8407ea7118d869061291f746fb2ffd2a8564e
haerin7427 <43716537+haerin7427@users.noreply.github.com>
2022-02-02 20:46:09 +0900

    LV1 math16 edit!!

--- src/programmers/Lv1_math16.java
+++ src/programmers/Lv1_math16.java
@@ -6,5 +6,27 @@ package programmers;
 // Create by haerin on 2022/02/01
 
 public class Lv1_math16 {
-    
+    class Solution {
+        public int solution(String s) {
+
+            int isNegative = 0;
+            if(s.charAt(0) == '-'){
+                isNegative = 1;
+                s = s.substring(1);
+            }else if(s.charAt(0) == '+')
+                s = s.substring(1);
+
+            int answer = 0;
+            for(int i = 0; i < s.length(); i++){
+                int num = s.charAt(i) - '0';
+                num *= Math.pow(10, s.length()-i-1);
+                answer += num;
+            }
+            
+            if(isNegative == 1)
+                answer *= -1;
+            
+            return answer;
+        }
+    }
 }
