6bde699232dd8fd9958ad8f7a165057d6626b591
haerin7427 <hae7427@gmail.com>
2022-03-22 23:58:56 +0900

    LV2 bigNumber complete!!

--- src/programmers/LV2_java/LV2_bigNumber.java
+++ src/programmers/LV2_java/LV2_bigNumber.java
@@ -1,57 +1,37 @@
 package programmers.LV2_java;
+import java.util.*;
 
 // https://programmers.co.kr/learn/courses/30/lessons/42883
 // title : 큰 수 만들기
-// time : 27m + ??
-// Create by haerin on 2022/03/21
+// time : 45m
+// Create by haerin on 2022/03/22
 
 public class LV2_bigNumber {
 
     class Solution {
         public String solution(String number, int k) {
-            String answer = "";
+            StringBuilder answer = new StringBuilder();
+            int startIdx = 0;
             
-            for(int i=0; i<k; i++){
-                String limit = findBigNumber(number);
+            for(int limit = k; limit<number.length(); limit++){
                 
-                if(number.substring(0,1).equals(limit))
-                    number = removeMinNumberWithLimit(number, "");
-                else
-                    number = removeMinNumberWithLimit(number, limit);
+                char maxValue = number.charAt(startIdx);
+                int maxIdx = startIdx;
+                
+                for(int i=startIdx+1; i<=limit; i++){
+                    if(maxValue < number.charAt(i)){
+                        maxValue = number.charAt(i);
+                        maxIdx = i;
+                    }
+                }
+                
+                answer.append(maxValue);
+                startIdx = maxIdx+1;
+                
             }
-            return number;
+            return answer.toString();
         }
         
-        public String findBigNumber(String number){
-            
-            int idx=0; String max = number.substring(0,1);
-                
-            for(int h=1; h<number.length(); h++)
-                if(Integer.parseInt(max) < Integer.parseInt(number.substring(h,h+1))){
-                    idx = h;
-                    max = number.substring(h,h+1);
-            }
-            return number.substring(idx,idx+1);
-            
-        }
-        
-        public String removeMinNumberWithLimit(String number, String limit){
-            
-            
-            int max=1; int idx=0; char min = number.charAt(0);    
-            for(int h=1; h<number.length(); h++){
-                if(number.substring(h,h+1).equals(limit)){
-                    max = h;
-                    break;
-                }
-                if(min > number.charAt(h)){
-                    idx = h;
-                    min = number.charAt(h);
-                }
-            }
-            number = number.substring(0,idx) + number.substring(idx+1);
-            return number; 
-        }
     }
     
 }
