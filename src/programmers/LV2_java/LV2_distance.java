1f833fceaca6815c4ac3b6eb2b502f4e641157b2
haerin7427 <hae7427@gmail.com>
2022-05-04 23:56:23 +0900

    distance complete!!

---
+++ src/programmers/LV2_java/LV2_distance.java
@@ -0,0 +1,14 @@
+package programmers.LV2_java;
+
+// https://programmers.co.kr/learn/courses/30/lessons/81302
+// title : 거리두기 확인하기
+// time : 
+// Create by haerin on 2022/05/04
+
+public class LV2_distance {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
