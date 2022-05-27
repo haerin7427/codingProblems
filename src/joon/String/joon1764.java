37d103bfde8eb003bbafa7b689d50114c4d140c1
haerin7427 <hae7427@gmail.com>
2022-05-27 23:13:17 +0900

    joon1764 complete!!

---
+++ src/joon/String/joon1764.java
@@ -0,0 +1,41 @@
+package joon.String;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+public class joon1764 {
+
+    public static void main(String[] args) throws IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        HashSet<String> set = new HashSet<>();
+        for(int i=0; i<N; i++)
+            set.add(br.readLine());
+
+        ArrayList<String> list = new ArrayList<>();
+        for(int i=0; i<M; i++){
+            String str = br.readLine();
+            if(set.contains(str)){
+                list.add(str);
+            }
+        }
+        Collections.sort(list);
+
+        StringBuffer sb = new StringBuffer();
+        sb.append(String.valueOf(list.size()));
+        for(String str : list)
+            sb.append("\n"+str);
+
+        System.out.println(sb);
+    }
+}
