56fa82902f285c46db75ffebd9451dfda7b9e749
haerin7427 <hae7427@gmail.com>
2022-05-29 00:27:32 +0900

    joon17609 complete!!

--- src/joon/String/joon17609.java
+++ src/joon/String/joon17609.java
@@ -4,44 +4,55 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+
+// https://www.acmicpc.net/problem/17609
+// title : 회문
+// time : 30m
+// Create by haerin on 2022/05/28 + Google
+
 public class joon17609 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int T = Integer.parseInt(br.readLine()); 
-        String[] str = new String[T];
-        for(int i=0; i<str.length; i++)
-            str[i] = br.readLine();
+        String[] words = new String[T];
+        for(int i=0; i<words.length; i++)
+            words[i] = br.readLine();
 
-        for(int i=0; i<str.length; i++){
-            int answer = 0;
-            String word = str[i];
-
-            int pre_idx = 0;
-            int post_idx = word.length()-1;
-
-            while(pre_idx < post_idx){
-                if(word.charAt(pre_idx) == word.charAt(post_idx)){
-                    pre_idx+=1;
-                    post_idx-=1;
-                }else if(answer == 0 && pre_idx+1<=post_idx && word.charAt(pre_idx+1) == word.charAt(post_idx)){
-                    answer = 1;
-                    pre_idx+=2;
-                    post_idx-=1;
-
-                }else if(answer == 0 && pre_idx<=post_idx-1 && word.charAt(pre_idx) == word.charAt(post_idx-1)){
-                    answer = 1;
-                    pre_idx+=1;
-                    post_idx-=2;
-                }else{
-                    answer = 2;
-                    break;
-                }
-            }
-            System.out.println(answer);
+        for(int i=0; i<words.length; i++){
+            if(check(words[i], 0, words[i].length()-1)) System.out.println(0);
+            else if(check_ver2(words[i], 0, words[i].length()-1)) System.out.println(1);
+            else System.out.println(2);
         }
 
+    }
+    public static boolean check(String word, int pre_idx, int post_idx){
+
+        while(pre_idx <= post_idx){
+            if(word.charAt(pre_idx) == word.charAt(post_idx)){
+                pre_idx+=1;
+                post_idx-=1;
+            }else{
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean check_ver2(String word, int pre_idx, int post_idx){
+
+        while(pre_idx <= post_idx){
+            if(word.charAt(pre_idx) == word.charAt(post_idx)){
+                pre_idx+=1;
+                post_idx-=1;
+            }else{
+                if(check(word, pre_idx+1, post_idx) || check(word, pre_idx, post_idx-1))
+                    return true;
+                else break;
+            }
+        }
+        return false;
     }
     
 }
