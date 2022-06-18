28047889d8e73f8e8fcaf6f5277e90831df505c6
haerin7427 <hae7427@gmail.com>
2022-06-18 22:55:45 +0900

    joystick complete!!

--- src/programmers/LV2_java/LV2_joystick.java
+++ src/programmers/LV2_java/LV2_joystick.java
@@ -2,45 +2,32 @@ package programmers.LV2_java;
 
 // https://programmers.co.kr/learn/courses/30/lessons/42860
 // title : 조이스틱
-// time : 30m + ...
-// Created by haerin on 2022/06/17
+// time : 30m + help
+// Created by haerin on 2022/06/18
 
 public class LV2_joystick {
 
     class Solution {
         public int solution(String name) {
-            int answer = 0;
-            char[] now = new char[name.length()];
-            for(int i=0; i<now.length; i++){
-                now[i] = 'A';
-            }
             
-            int idx = 0;
-            while(true){
+            int answer = 0;
+            int move = name.length() - 1;
+            
+            for(int i = 0; i < name.length(); i++) {
+                answer += Math.min(name.charAt(i) - 'A', 26 - (name.charAt(i) - 'A'));
                 
-                if(name.charAt(idx) != now[idx]){
-                    int option_up = name.charAt(idx) - 'A';
-                    int option_down = 1 + ('Z' - name.charAt(idx));
-                    answer += Math.min(option_up,option_down);
-                    now[idx] = name.charAt(idx);
+                if (i < name.length() - 1 && name.charAt(i + 1) == 'A') {
+                    int A_endIdx = i + 1;
+                    while(A_endIdx < name.length() && name.charAt(A_endIdx) == 'A')
+                        A_endIdx++;
+                    
+                    move = Math.min(move, i * 2 + (name.length() - A_endIdx));
+                    move = Math.min(move, i + (name.length() - A_endIdx) * 2);
                 }
-                
-                if(String.valueOf(now).equals(name)) break;
-                
-                int op1_next_idx = idx + 1;
-                int op2_next_idx = idx - 1;
-                
-                if(op1_next_idx >= now.length) op1_next_idx = 0;
-                if(op2_next_idx < 0) op2_next_idx = now.length - 1;
-                
-                if(name.charAt(op1_next_idx) == 'A'){
-                    idx = op2_next_idx;
-                }else
-                    idx = op1_next_idx;
-                answer += 1;          
             }
+            answer += move;
+            
             return answer;
         }
     }
-    
 }
