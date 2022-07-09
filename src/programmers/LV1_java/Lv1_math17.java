e4a4a12d59c333c437f007a7a5aa56bbe798a104
haerin7427 <hae7427@gmail.com>
2022-07-09 21:25:17 +0900

    math17 update!!

--- src/programmers/LV1_java/Lv1_math17.java
+++ src/programmers/LV1_java/Lv1_math17.java
@@ -1,33 +1,30 @@
 package programmers.LV1_java;
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.*;
 
 // https://programmers.co.kr/learn/courses/30/lessons/12910
 // title : 나누어 떨어지는 숫자 배열
-// time : 10m
+// time : 5m
 // Create by haerin on 2022/02/02
+// Update by haerin on 2022/07/09
 
 public class Lv1_math17 {
-
-    
-
-class Solution {
-    public int[] solution(int[] arr, int divisor) {
-        ArrayList<Integer> list = new ArrayList<Integer>();
-        for(int num : arr){
-            if(num % divisor == 0)
-                list.add(num);
+    class Solution {
+        public int[] solution(int[] arr, int divisor) {
+            List<Integer> list = new ArrayList<>();
+            for(int num : arr){
+                if(num%divisor == 0){
+                    list.add(num);
+                }
+            }
+            
+            if(list.size() == 0) return new int[]{-1};
+            
+            int[] answer = new int[list.size()];
+            int idx=0;
+            for(int num : list)
+                answer[idx++] = num;
+            Arrays.sort(answer);
+            return answer;
         }
-        if(list.size() == 0)
-            list.add(-1);
-        
-        int[] answer = new int[list.size()];
-        int idx = 0;
-        for(int num : list)
-            answer[idx++] = num;
-        Arrays.sort(answer);
-        return answer;
     }
-}
-    
 }
