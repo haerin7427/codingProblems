1cb1de771e007a5fd752685b9b15d4d76df5bdc1
haerin7427 <hae7427@gmail.com>
2022-07-17 20:48:12 +0900

    Q258 completegit add ./src/leetcode/Q258.java

--- src/leetcode/Q258.java
+++ src/leetcode/Q258.java
@@ -1,2 +1,23 @@
-package leetcode;public class Q258 {
+package leetcode;
+
+// https://leetcode.com/problems/add-digits/
+// title : 258. Add Digits
+// time : 5m
+// Created by haerin on 2022/07/17
+
+public class Q258 {
+    class Solution {
+        public int addDigits(int num) {
+
+            while(num > 9){
+                String s = String.valueOf(num);
+                int sum = 0;
+                for(char c : s.toCharArray())
+                    sum += c - '0';
+                num = sum;
+            }
+
+            return num;
+        }
+    }
 }
