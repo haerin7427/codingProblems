f243e0a5ad8f92e5665fe1a9fc973719f77d2550
haerin7427 <hae7427@gmail.com>
2022-07-23 23:04:19 +0900

    Q69 complete!!

---
+++ src/leetcode/Q69.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+// https://leetcode.com/problems/sqrtx/
+// title : 69. Sqrt(x)
+// time : 1m
+// Created by haerin on 22/07/23
+
+public class Q69 {
+    class Solution {
+        public int mySqrt(int x) {
+            int num = (int)Math.sqrt(x);
+            return num;
+        }
+    }
+}
