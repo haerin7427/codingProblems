519419e4e21f041fe1c7ce99ccddafadf91e1264
haerin7427 <hae7427@gmail.com>
2023-01-06 23:21:30 +0900

    [Leet] Invert Binary Tree

---
+++ src/grind75/week1No6.java
@@ -0,0 +1,35 @@
+package grind75;
+// https://leetcode.com/problems/invert-binary-tree/
+// title : 226. Invert Binary Tree
+// time : 30m + help
+// Create by haerin on 2023/01/06
+public class week1No6 {
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     *     int val;
+     *     TreeNode left;
+     *     TreeNode right;
+     *     TreeNode() {}
+     *     TreeNode(int val) { this.val = val; }
+     *     TreeNode(int val, TreeNode left, TreeNode right) {
+     *         this.val = val;
+     *         this.left = left;
+     *         this.right = right;
+     *     }
+     * }
+     */
+    class Solution {
+        public TreeNode invertTree(TreeNode root) {
+            if(root == null) return root;
+
+            invertTree(root.left);
+            invertTree(root.right);
+
+            TreeNode curr = root.left;
+            root.left = root.right;
+            root.right = curr;
+            return root;
+        }
+    }  
+}
